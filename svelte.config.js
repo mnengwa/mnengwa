a6b7aebd92499dd7819cefb2e1cabf45752cd566
mnengwa <howard@mnengwa.com>
2023-05-27 10:11:49 +0300

    feat: setup Tailwind css

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess as preprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         adapter: adapter()
-    }
+    },
+    preprocess: preprocess()
 };
 
 export default config;
